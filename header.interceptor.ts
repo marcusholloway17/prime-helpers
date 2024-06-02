1ca9a97e61f6dabc88edebec7baf3a93033a2e3d
marcusholloway17 <loganjean17@gmail.com>
2024-06-02 10:59:55 +0000

    update header interceptor

--- header.interceptor.ts
+++ header.interceptor.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@angular/core';
+import { Inject, Injectable } from "@angular/core";
 import {
   HttpRequest,
   HttpHandler,
@@ -6,21 +6,21 @@ import {
   HttpInterceptor,
   HTTP_INTERCEPTORS,
   HttpHeaders,
-} from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
-import { environment } from 'src/environments/environment.development';
-import { AUTH_SERVICE } from '../views/auth/types';
-import { AuthService } from '../views/auth/services/auth.service';
+} from "@angular/common/http";
+import { Observable, tap } from "rxjs";
+import { environment } from "src/environments/environment.development";
+import { AUTH_SERVICE } from "../auth/types";
+import { AuthService } from "../auth/services/auth.service";
 
 @Injectable()
 export class HeaderInterceptor implements HttpInterceptor {
-  public authToken!: string;
+  public cardRef!: string;
   constructor(@Inject(AUTH_SERVICE) private authService: AuthService) {
     this.authService.signInState$
       .pipe(
         tap((state) => {
-          if (state && state?.authToken) {
-            this.authToken = state.authToken;
+          if (state && state?.card?.cardRef) {
+            this.cardRef = state?.card?.cardRef;
           }
         })
       )
@@ -31,26 +31,22 @@ export class HeaderInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    if (/10000/.test(request.url) || /third-parties/.test(request.url)) {
-      const headers = new HttpHeaders().set(
-        'authorization',
-        request.headers.get('authorization') ?? `Bearer ${this.authToken}` ?? ''
+    const headers = new HttpHeaders()
+      .set(
+        "x-client-id",
+        this.authService.clientId ?? environment.auth.clientId
+      )
+      .set(
+        "x-client-secret",
+        this.authService.clientSecret ?? environment.auth.clientSecret
       );
-
-      request = request.clone({ headers });
+    if (this.authService.isLoggedIn()) {
+      headers.append(
+        "x-card-ref",
+        request.headers.get("x-card-ref") ?? this.cardRef
+      );
     }
-    if (/auth/.test(request.url)) {
-      const headers = new HttpHeaders()
-        .set('x-client-id', environment.auth.clientId)
-        .set('x-client-secret', environment.auth.clientSecret)
-        .set(
-          'authorization',
-          request.headers.get('authorization') ?? `Bearer ${this.authToken}`
-        );
-
-      request = request.clone({ headers });
-    }
-
+    request = request.clone({ headers });
     return next.handle(request);
   }
 }
