533f114b642ae47027bd4ef6ad598920bf20c1a6
marcusholloway17 <loganjean17@gmail.com>
2024-06-03 19:40:03 +0000

    theme service updated

--- theme.service.ts
+++ theme.service.ts
@@ -1,6 +1,8 @@
 import { DOCUMENT } from "@angular/common";
 import { Inject, Injectable } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { SessionStorageService } from "./session-storage.service";
+import { LocalStorageService } from "./local-storage.service";
 
 @Injectable({
   providedIn: "root",
@@ -12,7 +14,10 @@ export class ThemeService {
   private _theme$ = new BehaviorSubject<string>(this.getDefaultBrowserTheme());
   public theme$ = this._theme$.asObservable();
 
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private localStorageService: LocalStorageService
+  ) {}
 
   getTheme() {
     return this._theme$.getValue();
@@ -24,26 +29,33 @@ export class ThemeService {
       "app-theme"
     ) as HTMLLinkElement;
     if (themeLink) {
+      this.localStorageService.setItem("theme", theme);
       themeLink.href = theme + ".css";
     }
   }
 
   public getDefaultBrowserTheme() {
-    if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
-      this.switch(this.darkTheme);
-      return this.darkTheme;
-    } else if (
-      window.matchMedia &&
-      window.matchMedia("(prefers-color-scheme: light)").matches
-    ) {
-      this.switch(this.lightTheme);
-      return this.lightTheme;
+    const currentTheme = JSON.parse(this.localStorageService.getItem("theme"));
+    if (!currentTheme) {
+      if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+      ) {
+        this.switch(this.darkTheme);
+        return this.darkTheme;
+      } else if (
+        window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: light)").matches
+      ) {
+        this.switch(this.lightTheme);
+        return this.lightTheme;
+      } else {
+        this.switch(this.lightTheme);
+        return this.lightTheme;
+      }
     } else {
-      this.switch(this.lightTheme);
-      return this.lightTheme;
+      this.switch(currentTheme);
+      return currentTheme;
     }
   }
 }
